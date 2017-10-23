db598512d6d5ba8e01b517ccf4fea2d75e48863c
hungyuhang <f74036093@mail.ncku.edu.tw>
2017-10-23 18:32:55 +0800

    修改convert.php(變成每個你 我 他後面都會加上"阿嬤")

--- public/convert.php
+++ public/convert.php
@@ -4,12 +4,23 @@ $movie_name = $_REQUEST["movie_name"];
 $words = array("我", "妳", "你", "他", "她", "它", "牠");
 $result = $movie_name;
 
-foreach($words as $word){
-  $pos = strpos($movie_name, $word);
-  if($pos !== false){
-    # a character of traditional chinese is 3 bytes ,so is +3
-    $result = substr($movie_name, 0, $pos+3) . "阿嬤" . substr($movie_name, $pos+3);
-    break;
+#foreach($words as $word){
+#  $pos = strpos($movie_name, $word);
+#  if($pos !== false){
+#    # a character of traditional chinese is 3 bytes ,so is +3
+#    $result = substr($movie_name, 0, $pos+3) . "阿嬤" . substr($movie_name, $pos+3);
+#    break;
+#  }
+#}
+
+for($index = mb_strlen($result, "utf-8")-1; $index >= 0; $index--){
+  $w = mb_substr($result, $index, 1, "utf-8");
+  foreach($words as $word){
+    if($w === $word){
+      $result = mb_substr($result, 0, $index+1, "utf-8") . 
+          "阿嬤" . mb_substr($result, $index+1, null, "utf-8");
+      break;
+    }
   }
 }
 
